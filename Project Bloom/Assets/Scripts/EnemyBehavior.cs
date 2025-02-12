90c7f13d571207d2f0b07e8df685af2ad6dd9442
jyalto <161365560+jyalto@users.noreply.github.com>
2025-02-12 09:39:13 -0800

    Basic Enemy

--- Project Bloom/Assets/Scripts/EnemyBehavior.cs
+++ Project Bloom/Assets/Scripts/EnemyBehavior.cs
@@ -7,39 +7,54 @@ public class EnemyBehavior : MonoBehaviour
 {
     public Transform PatrolRoute;
     public List<Transform> Locations;
-    private int locationIndex = 0;
+    private int locationIndex;
 
     private NavMeshAgent agent;
 
+    public Transform player;
+
+
+    // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        player = GameObject.Find("Player").transform;
 
         InitializePatrolRoute();
         MoveToNextPatrolLocation();
     }
 
+    // Update is called once per frame
     void Update()
     {
-
         if (agent.remainingDistance < 0.2f && !agent.pathPending)
-        {
             MoveToNextPatrolLocation();
-        }
     }
     void InitializePatrolRoute()
     {
         foreach (Transform child in PatrolRoute)
-        {
             Locations.Add(child);
-        }
     }
     void MoveToNextPatrolLocation()
     {
-        //if (Locations.Count == 0)
-        //    return;
+        // We wouldn't want to use this of course because we want mobs to approach/move in a stright line then fight
+        if (Locations.Count == 0)
+            return;
 
         agent.destination = Locations[locationIndex].position;
-        //locationIndex = (locationIndex + 1) % Locations.Count;
+        locationIndex = (locationIndex + 1) % Locations.Count;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            agent.destination = player.position;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
     }
 }
